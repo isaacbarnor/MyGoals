7410b4a37784a0f83d8656461456b9726682b6a6
isaacbarnor <isaacbarnor@users.noreply.github.com>
2019-04-08 10:10:11 +0000

    Initial Change

---
+++ app/src/main/java/com/blueorb/mygoals/MainActivity.java
@@ -0,0 +1,23 @@
+package com.blueorb.mygoals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //  TODO: Declare member variables here
+
+
+
+    // TODO: Declare array here
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
